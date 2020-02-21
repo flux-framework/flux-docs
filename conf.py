9d681b59ab0e59f62c16125521c29722cf0e2612
Elsa Gonsiorowski (Uranus) <gonsiorowski1@llnl.gov>
2020-02-21 15:15:10 -0800

    conf.py: added stuff from RFC

--- conf.py
+++ conf.py
@@ -43,6 +43,14 @@ release = '0.13.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinxcontrib.spelling',
+    'recommonmark',
+]
+
+# sphinxcontrib.spelling settings
+spelling_word_list_filename = [
+    'spell.en.pws'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -51,10 +59,13 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'flux-core', 'README.md']
 
 master_doc = 'index'
-source_suffix = '.rst'
+source_suffix = {
+    '.rst' : 'restructuredtext',
+    '.md' : 'markdown',
+}
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -67,9 +78,20 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = [
+]
 
 # -- Options for man output -------------------------------------------------
 
 man_pages = [
 ]
+
+# -- Options for recommonmark -----------------------------------------------
+def on_missing_reference(app, env, node, contnode):
+        if node['reftype'] == 'any':
+                return contnode
+        else:
+                return None
+
+def setup(app):
+        app.connect('missing-reference', on_missing_reference)
