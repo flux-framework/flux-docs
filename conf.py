d8322eaf212975c887e7955ca55d1f122fd393a2
Mark A. Grondona <mark.grondona@gmail.com>
2026-06-04 06:14:06 -0700

    conf.py: add merriam-webster.com to linkcheck_ignore
    Problem: In CI the merriam-webster link for workflow gets
    
     403 Client Error: Forbidden for url
    
    The link still works outside of the CI environment, so add the
    URL to linkcheck_ignore.

--- conf.py
+++ conf.py
@@ -385,5 +385,6 @@ def setup(app):
 
 linkcheck_ignore = [
     r'https://github.com/flux-framework/flux-core\?tab\=readme-ov-file\#build-requirements',
-    r'https://www.mcs.anl.gov/papers/P1760.pdf'
+    r'https://www.mcs.anl.gov/papers/P1760.pdf',
+    r'https://www.merriam-webster.com/dictionary'
 ]
